c0d5a70220f56c55eb01f33e3328ae30bbe6cba3
Itejada <ismael.elpuig@gmail.com>
2019-04-10 19:09:01 +0200

    In progress

--- core/src/com/mygdx/model/Tablero.java
+++ core/src/com/mygdx/model/Tablero.java
@@ -31,7 +31,10 @@ public class Tablero {
         for (int x = 0; x <= 6; x++) {
 
             for (int y = 0; y <= 8; y++) {
-                casillas[y][x] = new Casilla(Casilla.getWIDTH() * y, Casilla.getHEIGHT() * x,imagenCasilla );
+                casillas[x][y].setTextureCasilla(assetManager.manager.get(assetManager.imageSquare, Texture.class));
+                casillas[x][y].setImageCasilla(new Image(casillas[x][y].getTextureCasilla()));
+
+                casillas[y][x] = new Casilla(Casilla.getWIDTH() * y, Casilla.getHEIGHT() * x,imagenCasilla, assetManager.manager.get(assetManager.imageSquare, Texture.class) );
                 casillas[y][x].setInvocation(casillaInvocacion);
                 tablaTablero.addActor(casillas[y][x].getImagenCasilla());
 
